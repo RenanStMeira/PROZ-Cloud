a358d1d6841dfb0a08119d9d57a887d703164420
RenanStMeira <renan.meira25@gmail.com>
2024-09-26 21:14:56 -0300

    feat: criado array deelementos

---
+++ python/Array_Elemenntos/main.py
@@ -0,0 +1,6 @@
+produtos_loja_musica = ["Guitarra", "Bateria", "Teclado", "Violão", "Microfone"]
+
+anos_nascimento = [1985, 1990, 1995, 2000, 2005]
+
+print("Produtos da loja de música:", produtos_loja_musica)
+print("Anos de nascimento de familiares e amigos:", anos_nascimento)
